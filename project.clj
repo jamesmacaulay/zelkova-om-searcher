56738f05c69778014f3db7f0ec8b546154eb8f35
James MacAulay <james@shopify.com>
2015-04-19 15:18:22 -0700

    update deps and add zelkova, nrepl-port config

--- project.clj
+++ project.clj
@@ -4,10 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+                 [org.clojure/clojurescript "0.0-3196"]
                  [figwheel "0.2.5-SNAPSHOT"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [jamesmacaulay/zelkova "0.4.0-SNAPSHOT" :exclusions [org.clojure/clojure
+                                                                      org.clojure/clojurescript
+                                                                      org.clojure/core.async]]
                  [sablono "0.3.4"]
                  [org.omcljs/om "0.8.8"]]
 
@@ -42,7 +45,7 @@
              :css-dirs ["resources/public/css"] ;; watch and update CSS
 
              ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
+             :nrepl-port 7888
 
              ;; Server Ring Handler (optional)
              ;; if you want to embed a ring handler into the figwheel http-kit
